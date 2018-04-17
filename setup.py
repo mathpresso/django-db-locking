e47b1b9e41dae738a3a964aa89ab094d721a6a93
jeongseongkyun <sunken37@gmail.com>
2018-04-17 17:56:40 +0900

    Use correct paths for 9.0

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
-from pip._internal.req.req_file import parse_requirements
-from pip._internal.download import PipSession
+try: # for pip >= 10
+    from pip._internal.req.req_file import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    from pip.download import PipSession
+
 import locking
 
 from os import path
